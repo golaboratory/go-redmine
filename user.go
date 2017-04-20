cbfab5ce7e99172af7e51354959f639ec0209729
Go Akahane <akahane@golaboratory.jp>
2017-04-20 14:52:26 +0900

    CurrentUser method added

--- user.go
+++ user.go
@@ -73,3 +73,27 @@ func (c *Client) User(id int) (*User, error) {
 	}
 	return &r.User, nil
 }
+
+func (c *Client) CurrentUser() (*User, error) {
+	res, err := c.Get(c.endpoint + "/users/current.json?key=" + c.apikey)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var r userResult
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &r.User, nil
+}
